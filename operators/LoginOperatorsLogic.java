5a3e6f2a8f0e5df2ccfb5f9dc4c17e8739f1f261
Hernani Gonzalez <hernanigonzalez.public@proton.me>
2025-04-10 10:13:47 -0500

    Array Login APp

--- operators/LoginOperatorsLogic.java
+++ operators/LoginOperatorsLogic.java
@@ -7,8 +7,11 @@ public class LoginOperatorsLogic {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        boolean loginUserBasic = loginUserBasic(scan);
-        System.out.println("loginUserBasic = " + loginUserBasic);
+//        boolean loginUserBasic = loginUserBasic(scan);
+//        System.out.println("loginUserBasic = " + loginUserBasic);
+
+        boolean loginUserArray = loginUserArray(scan);
+        System.out.println("loginUserArray = " + loginUserArray);
     }
 
     public static Boolean loginUserBasic(Scanner scan) {
@@ -40,7 +43,38 @@ public class LoginOperatorsLogic {
         scan.nextLine(); // Clean Buffer
         return isAuth;
     }
+    public static boolean loginUserArray(Scanner scan){
+        String[] usernames = new String[2];
+        String[] passwords = new String[2];
+        usernames[0] = "Jhon Doe";
+        passwords[0] = "12345";
 
+        usernames[1] = "Jhon";
+        passwords[1] = "123456";
+        // that is like = String[] usernames = {"Jhon Doe", "Jhon"}
+
+        System.out.println("Ingrese el usuario");
+        String u = scan.nextLine();
+
+        System.out.println("Ingrese la password");
+        String p = scan.nextLine();
+
+        boolean isAuth = false;
+
+        for (int i = 0; i < usernames.length; i++){
+            if(usernames[i].equals(u) && passwords[i].equals(p)){
+                isAuth = true;
+                break;
+            }
+        }
+
+        if (!isAuth){
+            System.out.println("Error, El usuario no existe");
+        }
+
+        scan.nextLine(); // Clean Buffer
+        return  isAuth;
+    }
 
 
 }
