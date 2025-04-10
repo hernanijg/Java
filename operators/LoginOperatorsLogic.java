b6b3092e7dde442154760f177f9edb1d7026b4b3
Hernani Gonzalez <hernanigonzalez.public@proton.me>
2025-04-10 10:05:28 -0500

    Simple Login app

--- operators/LoginOperatorsLogic.java
+++ operators/LoginOperatorsLogic.java
@@ -1,16 +1,22 @@
 package operators;
 
+import javax.swing.text.StyledEditorKit;
 import java.util.Scanner;
 
 public class LoginOperatorsLogic {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        boolean loginUserBasic = loginUserBasic(scan);
+        System.out.println("loginUserBasic = " + loginUserBasic);
+    }
+
+    public static Boolean loginUserBasic(Scanner scan) {
         String username = "Jhon Doe";
         String password = "12345";
 
         String username2 = "Jhon";
         String password2 = "123456";
-
-        Scanner scan = new Scanner(System.in);
 
         System.out.println("Ingrese el nombre de usuario: ");
         String u = scan.nextLine();
@@ -20,14 +26,21 @@ public class LoginOperatorsLogic {
 
         boolean isAuth = false;
 
-        if ( (username.equals(u) && password.equals(p))
-              ||  (username2.equals(u) && password2.equals(p)) ){
+        if ((username.equals(u) && password.equals(p))
+                || (username2.equals(u) && password2.equals(p))) {
             isAuth = true;
         }
-        if (isAuth){
+        
+        if (isAuth) {
             System.out.println("Bienvenido Usuario ".concat(u).concat("!"));
         } else {
             System.out.println("Acceso Restringido");
         }
+
+        scan.nextLine(); // Clean Buffer
+        return isAuth;
     }
+
+
+
 }
