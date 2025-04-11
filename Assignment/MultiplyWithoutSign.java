6132d90672624aba3fd4ef4645859092148278fc
Hernani Gonzalez <hernanigonzalez.public@proton.me>
2025-04-11 10:14:31 -0500

    MultiplyWithoutSign

---
+++ Assignment/MultiplyWithoutSign.java
@@ -0,0 +1,33 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class MultiplyWithoutSign {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Pase el numero a multiplicar: ");
+        int num1 = scan.nextInt();
+
+        System.out.println("Pase por cuanto lo vamos a multiplicar: ");
+        int num2 = scan.nextInt();
+
+        int result = 0;
+
+        // two Negatives
+        if (num1 < 0 && num2 < 0)
+            for (int i = 0; i > num2; i--)
+                result += -num1;
+        // once negative
+        else if (num2 < 0)
+            for (int i = 0; i > num2; i--)
+                result += -num1;
+        // two Positives
+        else for (int i = 0; i < num2; i++)
+                result += num1;
+
+
+        System.out.println("result = " + result);
+        System.exit(0);
+    }
+}
