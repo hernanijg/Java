153239f1c9540ef21b9a5892a9273ea0d4b10857
hernanijg <hernanigonzalez.public@proton.me>
2025-04-30 16:21:53 -0900

    Class: New Override(Equals) assign, Standard constructor and overcharge learn

--- customCLass/Absorb.java
+++ customCLass/Absorb.java
@@ -2,13 +2,23 @@ package customCLass;
 
 public class Absorb {
     public static void main(String[] args) {
-        Car newCar = new Car();
+        Car newCar = new Car("GT2", "Nissan");
+        Car newCar2 = new Car("GT2", "Nissan");
+        System.out.println(newCar);
         /*
-         * Because this two attributes are private, we need assigment by public methods
-         * We can't newCar.nameCar = "GT2" => That is a error
+         * The compare don't have access for object,
+         * The equals is change at the car.java file
          */
-        newCar.setNameCar("GT2");
-        newCar.setBrandCar("Nissan");
+        System.out.println(newCar == newCar2); // False
+        System.out.println(newCar.equals(newCar2)); // True
+        /*
+         * Because this two attributes are private
+         * we need assigment by public methods or configurate constructor
+         * We can't newCar.nameCar = "GT2" => That is a error
+         * for change values we can =>
+         */
+        newCar.setNameCar("GT3");
+        newCar.setBrandCar("Nissan Sport");
 
         // This line is possible for the Override
         System.out.println(newCar); // GT2 Nissan
--- customCLass/Car.java
+++ customCLass/Car.java
@@ -10,6 +10,20 @@ public class Car {
     private String brandCar;
 
     /*
+    * Standard Constructor
+    * This is a Standar so we don't need return nothing
+    * or assign avoid
+    * We can create overcharge method like this =>
+    */
+    public Car(String nameCar){
+        this.nameCar = nameCar;
+    }
+    public Car(String nameCar, String brandCar){
+        this.nameCar = nameCar;
+        this.brandCar = brandCar;
+    }
+
+    /*
     * The public methos can assign values to own private methods
     * and return that to another files, like Absorb.java
     * Use get... And set... Is Pojo and that is a standar in this case
@@ -35,5 +49,15 @@ public class Car {
     @Override
     public String toString() {
         return this.nameCar + " " + this.brandCar;
+    }
+
+    /* The real equal for objects
+     * We can't compare all object
+     * so we need compare the obj attributes
+     */
+    @Override
+    public boolean equals(Object obj){
+        Car a = (Car) obj; // Cast Object obj to Car obj
+        return (this.brandCar.equals(a.brandCar) && this.nameCar.equals(a.nameCar));
     }
 }
