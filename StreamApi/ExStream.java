c7c7af6b856cbb07c705aa07992c4bc5b69a7cd5
Hernani Gonzalez <hernanigonzalez.public@proton.me>
2025-05-26 10:49:00 -0500

    Stream Api define

---
+++ StreamApi/ExStream.java
@@ -0,0 +1,21 @@
+package StreamApi;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class ExStream {
+    public static void main(String[] args) {
+        //Stream<String> name = Stream.of("P", "J", "K", "L");
+
+        // String[] arr = {"P", "J", "K", "L"};
+        // Stream<String> name = Arrays.stream(arr);
+        // name.forEach(System.out::println);
+
+        Stream<String> name = Stream.<String>builder().add("P")
+            .add("J")
+            .add("K")
+            .add("L")
+            .build();
+        name.forEach(System.out::println);
+    }
+} 
